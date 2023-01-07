764cd6e51fbda47270521250f580b8ff02eb34e7
KimHuiMang <piyrw9754@gmail.com>
2023-01-07 14:06:28 +0900

    :memo: ErrorCode ErrorMessage 작성
    ErrorCode, ErrorMessage 작성

---
+++ src/main/java/lifestyle/awardscore/global/exception/ErrorMessage.java
@@ -0,0 +1,11 @@
+package lifestyle.awardscore.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorMessage {
+    private final String message;
+    private final int status;
+}
