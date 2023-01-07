ba1e6beb991b2886939fbcb33abf33b60748195d
KimHuiMang <piyrw9754@gmail.com>
2023-01-07 14:07:18 +0900

    :sparkles: 예외처리 핸들러 생성
    예외처리 핸들러 생성

---
+++ src/main/java/lifestyle/awardscore/global/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package lifestyle.awardscore.global.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private void printError(HttpServletRequest request, RuntimeException ex, String message) {
+        log.error(request.getRequestURI());
+        log.error(message);
+        ex.printStackTrace();
+    }
+}
