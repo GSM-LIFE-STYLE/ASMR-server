7c37653471257c59763dacb07463602d8ef6e368
KimHuiMang <piyrw9754@gmail.com>
2023-01-08 22:54:19 +0900

    :sparkles: 멤버 퍼사드의 유틸 메서드 추가
    멤버 퍼사드 유틸 추가

---
+++ src/main/java/lifestyle/awardscore/domain/auth/exception/PasswordMismatchException.java
@@ -0,0 +1,14 @@
+package lifestyle.awardscore.domain.auth.exception;
+
+import lifestyle.awardscore.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class PasswordMismatchException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public PasswordMismatchException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.MISMATCH_MEMBER_PASSWORD;
+    }
+}
--- src/main/java/lifestyle/awardscore/domain/member/facade/MemberFacade.java
+++ src/main/java/lifestyle/awardscore/domain/member/facade/MemberFacade.java
@@ -1,10 +1,12 @@
 package lifestyle.awardscore.domain.member.facade;
 
+import lifestyle.awardscore.domain.auth.exception.PasswordMismatchException;
 import lifestyle.awardscore.domain.member.entity.Member;
 import lifestyle.awardscore.domain.member.exception.MemberNotFoundException;
 import lifestyle.awardscore.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +14,22 @@ import org.springframework.stereotype.Component;
 public class MemberFacade {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member getCurrentMember() {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 회원입니다."));
+    }
+
+    public void checkPassword(Member member, String password) {
+        if(!passwordEncoder.matches(password, member.getPassword())) {
+            throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    public Long getUserId() {
+        return getCurrentMember().getMemberId();
     }
 
 }
