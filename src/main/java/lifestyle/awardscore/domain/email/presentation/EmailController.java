7e5577de54ef6f5a2d12fe6171e46ba008913f24
KimHuiMang <piyrw9754@gmail.com>
2023-01-09 18:16:10 +0900

    :sparkles: 이메일 인증번호 체크
    이메일 인증번호 체크

--- src/main/java/lifestyle/awardscore/domain/email/presentation/EmailController.java
+++ src/main/java/lifestyle/awardscore/domain/email/presentation/EmailController.java
@@ -1,17 +1,17 @@
 package lifestyle.awardscore.domain.email.presentation;
 
 import lifestyle.awardscore.domain.email.presentation.dto.request.EmailSendRequest;
+import lifestyle.awardscore.domain.email.service.EmailCheckService;
 import lifestyle.awardscore.domain.email.service.EmailSendService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +19,17 @@ import javax.validation.Valid;
 public class EmailController {
 
     private final EmailSendService emailSendService;
+    private final EmailCheckService emailCheckService;
 
     @PostMapping(name = "send", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> sendEmail(@Valid @RequestBody EmailSendRequest request) {
         emailSendService.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @RequestMapping(method = RequestMethod.HEAD)
+    public ResponseEntity<Void> mailCheck(@Email @RequestParam String email, @RequestParam String authKey) {
+        emailCheckService.execute(email, authKey);
+        return ResponseEntity.ok().build();
     }
 }
