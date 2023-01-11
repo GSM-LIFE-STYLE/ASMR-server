8281168690d1b895b93783996caed8763c133f87
KimHuiMang <piyrw9754@gmail.com>
2023-01-11 18:23:34 +0900

    :sparkles: 주문 내역 엔티티 작성
    주문 내역 엔티티

--- src/main/java/lifestyle/awardscore/domain/order/entity/OrderHistory.java
+++ src/main/java/lifestyle/awardscore/domain/order/entity/OrderHistory.java
@@ -1,4 +1,4 @@
-package lifestyle.awardscore.domain.shop.entity;
+package lifestyle.awardscore.domain.order.entity;
 
 import lifestyle.awardscore.domain.member.entity.Member;
 import lifestyle.awardscore.domain.product.entity.Product;
@@ -14,18 +14,30 @@ import java.util.List;
 @Getter @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Shop {
-
+public class OrderHistory {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "shop_id", nullable = false)
-    private Long shopId;
-
+    @Column(name = "order_history_id", nullable = false)
+    private Long orderHistoryId;
+    
     @OneToOne(mappedBy = "memberId")
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany
+    @OneToOne(mappedBy = "orderId")
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @OneToMany(mappedBy = "productId")
     @JoinColumn(name = "product_id")
     private List<Product> products;
+
+    @Column(name = "orderStatus")
+    private boolean orderStatus = false;
+
+    public void updateStatus() {
+        this.orderStatus = true;
+    }
+
 }
