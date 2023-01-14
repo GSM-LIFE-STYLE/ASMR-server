9cb0750a7e1f2fcbf198348e6378d513190c415e
KimHuiMang <piyrw9754@gmail.com>
2023-01-14 12:03:15 +0900

    :recycle: 상점마켓 api swagger 등록
    스웨거

--- src/main/java/lifestyle/awardscore/domain/shop/presentation/ShopController.java
+++ src/main/java/lifestyle/awardscore/domain/shop/presentation/ShopController.java
@@ -1,5 +1,8 @@
 package lifestyle.awardscore.domain.shop.presentation;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lifestyle.awardscore.domain.shop.presentation.dto.RegisterShopRequest;
 import lifestyle.awardscore.domain.shop.service.RegisterShopService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,11 @@ public class ShopController {
     private final RegisterShopService registerShopService;
 
     @PostMapping
+    @ApiOperation(value = "상점마켓 등록", notes = "상점마켓 등록")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
     public ResponseEntity<Void> registerShop(RegisterShopRequest request) {
         registerShopService.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
