76a2297a0daaf5ea5bbfa6dfb5a5bc1895c96058
JuuuuHong <wnghd720@naver.com>
2023-02-07 22:02:12 +0900

    :bug: @Id 수정

--- src/main/java/lifestyle/awardscore/domain/auth/entity/BlackList.java
+++ src/main/java/lifestyle/awardscore/domain/auth/entity/BlackList.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
-import javax.persistence.Id;
+
 
 @Getter @Builder
 @NoArgsConstructor
