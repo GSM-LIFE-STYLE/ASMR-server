4684891a1d69732a3343a3a3b278767062e0045d
JuuuuHong <wnghd720@naver.com>
2023-02-07 21:16:34 +0900

    :sparkles: Logout 구현

--- src/main/java/lifestyle/awardscore/domain/auth/service/MemberLogoutService.java
+++ src/main/java/lifestyle/awardscore/domain/auth/service/MemberLogoutService.java
@@ -1,9 +1,15 @@
 package lifestyle.awardscore.domain.auth.service;
 
 import lifestyle.awardscore.domain.auth.entity.BlackList;
+import lifestyle.awardscore.domain.auth.entity.RefreshToken;
 import lifestyle.awardscore.domain.auth.exception.BlackListAlreadyExistException;
+import lifestyle.awardscore.domain.auth.exception.NotFoundRefreshTokenException;
 import lifestyle.awardscore.domain.auth.repository.BlackListRepository;
+import lifestyle.awardscore.domain.auth.repository.RefreshTokenRepository;
+import lifestyle.awardscore.domain.member.entity.Member;
+import lifestyle.awardscore.domain.member.facade.MemberFacade;
 import lifestyle.awardscore.global.security.jwt.TokenProvider;
+import lifestyle.awardscore.global.security.jwt.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +19,9 @@ public class MemberLogoutService {
 
     private final BlackListRepository blackListRepository;
     private final TokenProvider tokenProvider;
+    private final MemberFacade memberFacade;
+    private final JwtProperties jwtProperties;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     private void saveBlackList(String email, String accessToken) {
 
@@ -27,5 +36,14 @@ public class MemberLogoutService {
                 .timeToLive(expiredTime)
                 .build();
         blackListRepository.save(blackList);
+    }
+
+    public void logout(String accessToken) {
+        Member member = memberFacade.getCurrentMember();
+        String email = tokenProvider.getUserEmail(accessToken, jwtProperties.getAccessSecret());
+        RefreshToken refreshToken = refreshTokenRepository.findById(email)
+                .orElseThrow(() -> new NotFoundRefreshTokenException("존재하지 않는 리프레시 토큰입니다."));
+        refreshTokenRepository.delete(refreshToken);
+        saveBlackList(member.getEmail(), accessToken);
     }
 }
