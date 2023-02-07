27ce75f9b1f990ffa9ca27a391202b93772d161c
JuuuuHong <wnghd720@naver.com>
2023-02-07 21:01:33 +0900

    :sparkles: BlackList entity 작성

---
+++ src/main/java/lifestyle/awardscore/domain/auth/entity/BlackList.java
@@ -0,0 +1,25 @@
+package lifestyle.awardscore.domain.auth.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import javax.persistence.Id;
+
+@Getter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash(value = "blackList")
+public class BlackList {
+
+    @Id
+    private String accessToken;
+
+    private String email;
+
+    @TimeToLive
+    private Long timeToLive;
+}
