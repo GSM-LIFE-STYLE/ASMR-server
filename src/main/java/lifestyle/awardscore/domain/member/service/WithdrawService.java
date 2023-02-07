79f0832c616e16f837992da393eaff34bc332d9b
aesperer <piyrw9754@gmail.com>
2023-02-07 20:26:53 +0900

    :sparkles: 회원 탈퇴 서비스 로직 작성
    로직 작성

---
+++ src/main/java/lifestyle/awardscore/domain/member/service/WithdrawService.java
@@ -0,0 +1,32 @@
+package lifestyle.awardscore.domain.member.service;
+
+import lifestyle.awardscore.domain.auth.entity.RefreshToken;
+import lifestyle.awardscore.domain.auth.exception.NotFoundRefreshTokenException;
+import lifestyle.awardscore.domain.auth.repository.RefreshTokenRepository;
+import lifestyle.awardscore.domain.member.entity.Member;
+import lifestyle.awardscore.domain.member.facade.MemberFacade;
+import lifestyle.awardscore.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class WithdrawService {
+
+    private final MemberFacade memberFacade;
+    private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public void execute(String password) {
+        Member member = memberFacade.getCurrentMember();
+        RefreshToken refreshToken = refreshTokenRepository.findById(member.getEmail())
+                .orElseThrow(() -> new NotFoundRefreshTokenException("존재하지 않는 리프레시 토큰입니다."));
+
+        memberFacade.checkPassword(member, password);
+
+        refreshTokenRepository.delete(refreshToken);
+        memberRepository.delete(member);
+    }
+}
