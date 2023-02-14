8b32913b9c389cb2233dcb8e058829c3aa193b92
aesperer <piyrw9754@gmail.com>
2023-02-15 01:40:48 +0900

    :sparkles: market path/item Item 등록 api 아카이브
    아카이브

--- src/main/java/lifestyle/awardscore/domain/market/presentation/teacher/TeacherMarketController.java
+++ src/main/java/lifestyle/awardscore/domain/market/presentation/teacher/TeacherMarketController.java
@@ -1,13 +1,13 @@
 package lifestyle.awardscore.domain.market.presentation.teacher;
 
+import lifestyle.awardscore.domain.item.presentation.dto.CreateItemRequest;
+import lifestyle.awardscore.domain.item.service.CreateItemService;
 import lifestyle.awardscore.domain.market.presentation.dto.request.CreateMarketRequest;
 import lifestyle.awardscore.domain.market.service.CreateMarketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,11 +17,18 @@ import javax.validation.Valid;
 public class TeacherMarketController {
 
     private final CreateMarketService createMarketService;
+    private final CreateItemService createItemService;
 
     @PostMapping("/register")
     public ResponseEntity<Void> createMarket(@Valid @RequestBody CreateMarketRequest request) {
         createMarketService.execute(request);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{marketId}/item")
+    public ResponseEntity<Void> createItem(@PathVariable Long marketId, @Valid @RequestBody CreateItemRequest request){
+        createItemService.execute(marketId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
