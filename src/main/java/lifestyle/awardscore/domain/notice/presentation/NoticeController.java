4472b461870a0a2a3f33c4cf43990828f01bda32
JuuuuHong <wnghd720@naver.com>
2023-02-15 23:15:45 +0900

    :sparkles: 게시글 조회 api

--- src/main/java/lifestyle/awardscore/domain/notice/presentation/NoticeController.java
+++ src/main/java/lifestyle/awardscore/domain/notice/presentation/NoticeController.java
@@ -6,10 +6,7 @@ import lifestyle.awardscore.domain.notice.service.WriteNoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,6 +22,12 @@ public class NoticeController {
     public ResponseEntity<Void> WriteNotice(@RequestBody @Valid WriteNoticeRequest request) {
         writeNoticeService.execute(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> ViewNotice() {
+        viewNoticeService.execute();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
