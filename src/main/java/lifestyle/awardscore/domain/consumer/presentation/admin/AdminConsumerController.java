bea63eeac15b44dc7dbe99e3df35c2c7e1a3ec96
aesperer <piyrw9754@gmail.com>
2023-02-23 17:38:16 +0900

    :sparkles: 마켓 나가기 api
    api

--- src/main/java/lifestyle/awardscore/domain/consumer/presentation/admin/AdminConsumerController.java
+++ src/main/java/lifestyle/awardscore/domain/consumer/presentation/admin/AdminConsumerController.java
@@ -1,22 +1,27 @@
 package lifestyle.awardscore.domain.consumer.presentation.admin;
 
+import lifestyle.awardscore.domain.consumer.service.ExitMarketService;
 import lifestyle.awardscore.domain.consumer.service.RegisterMarketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/consumer")
 public class AdminConsumerController {
     private final RegisterMarketService registerMarketService;
+    private final ExitMarketService exitMarketService;
 
     @PatchMapping("/{marketId}/register")
     public ResponseEntity<Void> registerMarket(@PathVariable Long marketId){
         registerMarketService.execute(marketId);
         return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{marketId}/exit")
+    public ResponseEntity<Void> exitMarket(@PathVariable Long marketId){
+        exitMarketService.execute(marketId);
+        return ResponseEntity.ok().build();
     }
 }
